6476d25fcfce8b5e66ec14585d4f6494c2df65b5
Samer Zagzoog <mrsamer.dev@gmail.com>
2020-08-12 09:08:54 +0300

    Initial setups & routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: HomeComponent, path: '' },
+  { component: AboutComponent, path: 'about' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PoliciesComponent } from './fga/policies/policies.component';
+import { TermsComponent } from './fga/terms/terms.component';
+import { ClientsComponent } from './fga/clients/clients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    PoliciesComponent,
+    TermsComponent,
+    ClientsComponent
   ],
   imports: [
     BrowserModule,
